94d9caf67c14dfb4580f44e8b44deb0c314729e6
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2017-09-30 02:15:50 +0200

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,17 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/extensions': 'off',
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }],
-    semi: ['error', 'never'],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': ['warn', { 'aspects': ['invalidHref'] }],
+    'semi': ['error', 'never'],
+    'linebreak-style': 0
   },
   'env': {
     'browser': true,
     'node': true,
+    'jest/globals': true
   },
+  'plugins': [
+      'jest'
+  ]
 };
