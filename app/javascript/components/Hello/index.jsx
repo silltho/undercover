85ae02b2d5d00f30c8c0fe8dba3e5a4b01ad1276
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2017-09-30 10:44:25 +0200

    fix errors

--- app/javascript/components/Hello/index.jsx
+++ app/javascript/components/Hello/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
@@ -14,3 +14,4 @@ Hello.propTypes = {
 }
 
 export default Hello
+
