71a3d4fb4aa5bdbccf724edb9f60f56f37c526fb
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2017-10-05 01:59:14 +0200

    removed devise, created identities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
 
   # GET /users
   # GET /users.json
@@ -65,13 +64,8 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = current_user
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      #params.require(:user).permit(:name, :surname, :birthdate, :email, :password, :description, :gender, :pic, :matchstring, :hobby_id, :latitude, :longitude, :address, :percent)
+      params.require(:user).permit(:email)
     end
 end
