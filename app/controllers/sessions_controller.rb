e242f80f4519bc1f507c0842122d00f293c7e774
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2017-10-05 02:17:54 +0200

    added logic in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,34 @@
 class SessionsController < ApplicationController
   def create
-    # Login the User here
+    uth = request.env['omniauth.auth']
+    @identity = Identity.find_or_create_omniauth(auth)
+
+    if signed_in?
+      # logged-in user tries to link different identitiy
+      if @identity.user != current_user
+        @identity.user = current_user
+        @identity.save
+      end
+    else
+      # user is not logged in
+      if @identity.user.present?
+      # login user with existing identity
+      @current_user = @identity.user
+      cookies.signed[:user_id] = @identity.user.id
+      else
+      # create identity for new user
+      @user = User.create_with_omniauth(auth)
+      @identity.user = @user
+      @identity.save
+      @current_user = @user
+      end
+    end
+    redirect_to root_path
   end
 
   def destroy
-    # Logout the User here
+    cookies.signed[:user_id] = nil
+    @current_user = nil
+    redirect_to root_path
   end
 end
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,11 +1,7 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
 
-  def self.find_with_omniauth(auth)
-    find_by(uid: auth['uid'], provider: auth['provider'])
-  end
-
-  def self.create_with_omniauth(auth)
-    create(uid: auth['uid'], provider: auth['provider'])
+  def self.find_or_create_omniauth(auth)
+    where(uid: auth['uid'], provider: auth['provider']).first_or_create
   end
 end
