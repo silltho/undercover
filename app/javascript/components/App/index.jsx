148c966575254442a05c1b71d4d929333ca87f47
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2017-10-23 20:15:05 +0200

    refactor frontend

--- app/javascript/components/App/index.jsx
+++ app/javascript/components/App/index.jsx
@@ -1,40 +1,68 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import ActionCable from 'actioncable'
+import consumer from 'services/'
 
 class App extends React.PureComponent {
-    constructor(props) {
-        super(props)
-        
-        const consumer = ActionCable.createConsumer()
-        consumer.subscriptions.create({channel: 'ApplicationChannel'}, {
-            connected: () => {
-                console.log('Cable connected')
-            },
-	          received: (data) => {
-                console.log(`receive data:`,data)
-            }
-        })
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      games: [],
+      isConnected: false
     }
 
+    consumer.subscriptions.create({ channel: 'ApplicationChannel' }, {
+      connected: this.handleConnected,
+      received: this.handleReceived,
+      disconnected: this.handleDisconnected
+    })
+  }
+
+  handleConnected = () => {
+    this.setState({
+      isConnected: true
+    })
+  }
+
+  handleReceived = (data) => {
+    this.setState({
+      currentUser: data.currentUser,
+      games: data.games
+    })
+  }
+
+  handleDisconnected = () => {
+    this.setState({
+      isConnected: false,
+      games: []
+    })
+  }
+
+  renderGame = (game) => (
+    <li>{game}</li>
+  )
+
   render() {
+    const games = this.state.games.map ? this.state.games.map((game) => this.renderGame(game)) : 'keine Games gefunden :('
+
     return (
       <div>
         <h2>Undercover App</h2>
         <hr />
-        <span>Hallo ... check console logs</span>
+        { this.state.currentUser && (
+        <div>
+		        Hallo {this.state.currentUser.email}
+          <hr />
+        </div>
+          )
+	      }
+        <h3>Games:</h3>
+        <ul>
+          {games}
+        </ul>
         <hr />
       </div>
     )
   }
-}
-
-App.defaultProps = {
-  user: { name: 'Default' }
-}
-
-App.propTypes = {
-  user: PropTypes.object
 }
 
 export default App
--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom'
 import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const node = document.getElementById('app-props')
-  const props = JSON.parse(node.getAttribute('data'))
-  console.log(props)
   ReactDOM.render(
-    <App {...props} />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
