300432d34f7293dc931bd97ee8474fcae7547654
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-01-30 12:32:50 +0100

    empty functions for game

--- app/models/game.rb
+++ app/models/game.rb
@@ -25,4 +25,49 @@ class Game < ApplicationRecord
   def log_status_change
     puts "Game #{self.id} '#{self.title}' changing from #{aasm.from_state} to #{aasm.to_state} (event: #{aasm.current_event})"
   end
+
+  def get_codename
+  end
+
+  def get_character
+  end
+
+  def get_relations
+    #after relations are clear, game starts
+    self.start
+  end
+
+  def update_ui
+  end
+
+  def start_timer
+    #gehört sowas ned eher ins frontend?
+  end
+
+  def get_news
+  end
+
+  def get_population
+  end
+
+  def get_mafia_members
+  end
+
+  def get_town_members
+  end
+
+  def get_processed_activities
+  end
+
+  def display_town
+  end
+
+  def save_results
+  end
+
+
+  private
+
+  def process_activities
+  end
 end
