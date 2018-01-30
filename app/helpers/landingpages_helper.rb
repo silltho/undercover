22caeccf5fe7e17e23a5ccab8e33990ac352bf1a
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-01-30 13:23:51 +0100

    Add Footer, Copyright Helper

--- app/helpers/landingpages_helper.rb
+++ app/helpers/landingpages_helper.rb
@@ -1,2 +1,17 @@
 module LandingpagesHelper
+
+  # https://rietta.com/blog/2011/12/26/how-to-automate-copyright-notice/
+  def copyright_notice_year_range(start_year)
+    start_year = start_year.to_i
+
+    current_year = Time.new.year
+
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
 end
