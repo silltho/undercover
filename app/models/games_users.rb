4fabcf5e2c820f0770b8a3d8b2646060a71a9a86
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-01-31 15:02:38 +0100

    user can only join game once

--- app/models/games_users.rb
+++ app/models/games_users.rb
@@ -1,10 +1,11 @@
 class GamesUsers < ApplicationRecord
   include AASM
-
   aasm :column => 'state', :whiny_transitions => false do
     state :alive, :initial => true
     state :dead, :disconnected, :imprisoned
   end
   belongs_to :game
   belongs_to :user
+  validates :user, uniqueness: { scope: :game,
+                                 message: "you can only join the same game once" }
 end
