da4fe57963cd2c7053e99249e1fd5a0982db0419
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-02-02 10:34:46 +0100

    Fix Form Button, Add redirect if 404

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :users
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/logout', to: 'sessions#destroy', via: [:get, :post], as: 'logout'
-
   get '/app', to: 'application#app'
 
   # Landing Page
@@ -14,4 +13,5 @@ Rails.application.routes.draw do
   get "/login_form", to: 'landingpages#login_form'
 
   root to: 'landingpages#index'
+  get '*path' => redirect('/')
 end
