9c0c6138beef0dde02bfa3c0b06975ccb77a3362
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2018-03-12 16:07:39 +0100

    change div to li

--- app/javascript/components/GameInfo/index.jsx
+++ app/javascript/components/GameInfo/index.jsx
@@ -27,7 +27,7 @@ class GameInfo extends React.PureComponent {
                   <ul>
                     {
                       infos.map((info) => (
-                        <div>{info}</div>
+                        <li>{info}</li>
                       ))
                     }
                   </ul>
