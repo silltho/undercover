c1fe9bf858ebaf5b81651361a167bad220af835e
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2018-03-15 19:05:44 +0100

    implement IconFont

--- app/javascript/components/GameExchange/index.jsx
+++ app/javascript/components/GameExchange/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Header from 'components/Header'
 import Content from 'components/Content'
 import Footer from 'components/Footer'
 import Button from 'components/Button'
+import IconFont, { ICONS } from 'components/IconFont'
 
 class GameExchange extends React.PureComponent {
   render() {
@@ -15,6 +15,7 @@ class GameExchange extends React.PureComponent {
       <React.Fragment>
         <Content>
           <div>Austausch-phase</div>
+          <IconFont icon={ICONS.home} />
         </Content>
         <Footer>
           <Button onClick={endExchange} text="done" />
