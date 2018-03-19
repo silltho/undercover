804e508a4bec3298e53b7ca1ac06af55ef1f79d6
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-03-19 13:39:18 +0100

    Prod

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,3 +1,22 @@
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
+
+const customConfig = {
+    plugins: [
+        new ServiceWorkerWebpackPlugin({
+            entry: './app/javascript/packs/registerServiceWorker.js'
+        }),
+    ]
+}
+
 const environment = require('./environment')
+
+// Merge custom config
+environment.config.merge(customConfig)
+
+/*
+environment.loaders.get('sass')
+    .use.find((item) => item.loader === 'sass-loader')
+        .options.includePaths = [resolve('app', 'javascript', 'styles')]
+*/
 
 module.exports = environment.toWebpackConfig()
