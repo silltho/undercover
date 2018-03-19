600c727e4cce9191c9425d38901d4b8b92df33a8
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2018-03-19 12:51:47 +0100

    implement service worker

--- app/assets/javascripts/login.es6.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-// $(".button-collapse").sideNav();
-
-$(document).ready(() => {
-  $('.play-game-button').on('click', () => {
-    $('.login-modal').removeClass('hidden')
-  })
-})
-
