1c0dd5bb4fd361d95876b3e2ca440a3badad7d4a
Thomas Siller <siller.thomas@gmx.net>
2018-04-06 09:26:29 +0200

    add clock to exchange screen

--- app/javascript/components/GameExchange/index.jsx
+++ app/javascript/components/GameExchange/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import IconFont, { ICONS } from 'components/IconFont'
+import styled from 'styled-components'
 import {
   BorderContainer,
   Content,
@@ -8,6 +8,78 @@ import {
   BorderContainerTitel,
   Action
 } from 'styles/components'
+
+
+const Clock = styled.div`
+/* -------------- Clock -------------- */
+
+  border-radius: 60px;
+  border: 3px solid #fff;
+  height: 80px;
+  width: 80px;
+  position: relative;
+
+  top: 28%;
+  top: -webkit-calc(50% - 43px);
+  top: calc(50% - 43px);
+  left: 35%;
+  left: -webkit-calc(50% - 43px);
+  left: calc(50% - 43px);
+  
+&:after{
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  top:2px;
+  left: 48%;
+  height: 38px;
+  width: 4px;
+  border-radius: 5px;
+  -webkit-transform-origin: 50% 97%;
+      transform-origin: 50% 97%;
+  -webkit-animation: grdAiguille 2s linear infinite;
+      animation: grdAiguille 2s linear infinite;
+}
+
+@-webkit-keyframes grdAiguille{
+    0%{-webkit-transform:rotate(0deg);}
+    100%{-webkit-transform:rotate(360deg);}
+}
+
+@keyframes grdAiguille{
+    0%{transform:rotate(0deg);}
+    100%{transform:rotate(360deg);}
+}
+
+&:before{
+  content: "";
+  position: absolute;
+  background-color: #fff;
+  top:6px;
+  left: 48%;
+  height: 35px;
+  width: 4px;
+  border-radius: 5px;
+  -webkit-transform-origin: 50% 94%;
+      transform-origin: 50% 94%;
+  -webkit-animation: ptAiguille 12s linear infinite;
+      animation: ptAiguille 12s linear infinite;
+}
+
+@-webkit-keyframes ptAiguille{
+    0%{-webkit-transform:rotate(0deg);}
+    100%{-webkit-transform:rotate(360deg);}
+}
+
+@keyframes ptAiguille{
+    0%{transform:rotate(0deg);}
+    100%{transform:rotate(360deg);}
+}
+`
+
+const ClockContainer = styled.div`
+  flex: 1;
+`
 
 
 class GameExchange extends React.PureComponent {
@@ -21,7 +93,9 @@ class GameExchange extends React.PureComponent {
         <Content>
           <BorderContainer>
             <BorderContainerTitel>Exchange</BorderContainerTitel>
-            <IconFont icon={ICONS.home} />
+            <ClockContainer>
+              <Clock />
+            </ClockContainer>
             <BorderContainerFooter>
               <Action onClick={endExchange}>
                 next
