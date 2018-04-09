c71706566626453295ca8f2e1ef67d2c83ea40e4
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-04-09 14:56:25 +0200

    refactoring user channel

--- app/channels/games_channel.rb
+++ app/channels/games_channel.rb
@@ -1,66 +1,63 @@
 class GamesChannel < ApplicationCable::Channel
   def subscribed
-    game = find_game
-    stream_for game
+    find_game
+    stream_for @game
   end
 
   def initialize_game
-    game = current_user.game
-    game.reload
-    game.initializing!
-    game.broadcast_game_updated
+    @game.reload
+    @game.initializing!
+    @game.broadcast_game_updated
   end
 
   def start_game
-    game = current_user.game
-    game.reload
-    game.started!
-    game.broadcast_game_updated
+    @game.reload
+    @game.started!
+    @game.broadcast_game_updated
   end
 
   def end_info_phase
-    game = current_user.game
-    game.reload
-    game.informed!
-    game.broadcast_game_updated
+    @game.reload
+    @game.informed!
+    @game.broadcast_game_updated
   end
 
   def end_exchange_phase
-    game = current_user.game
-    game.reload
-    game.exchanged!
-    game.broadcast_game_updated
+    @game.reload
+    @game.exchanged!
+    @game.broadcast_game_updated
   end
 
   def use_skill(params)
-    game = current_user.game
-    game.reload
-    game.use_skill(current_user.id, params['victim'])
+    @game.reload
+    @game.use_skill(current_player.id, params['victim'])
   end
 
   def all_skills_used
-    game = current_user.game
-    game.reload
-    game.skills_used!
-    game.broadcast_information_updated(game.round)
-    game.broadcast_game_updated
+    @game.reload
+    @game.skills_used!
+    @game.broadcast_information_updated(@game.round)
+    @game.broadcast_game_updated
   end
 
   def finish_game
-    game = current_user.game
-    game.reload
-    game.finish!
-    game.broadcast_game_updated
+    @game.reload
+    @game.finish!
+    @game.broadcast_game_updated
   end
 
   def reset_game
-    game = current_user.game
-    game.reload
-    game.reset!
+    @game.reload
+    @game.reset!
   end
 
   private
+
   def find_game
-    Game.where(code: params[:gamecode], aasm_state: 'waiting').first
+    @game = Game.where(code: params[:gamecode], aasm_state: 'waiting').first
+  end
+
+  def current_player
+    Player.where(game: @game, user: current_user).first
   end
 end
