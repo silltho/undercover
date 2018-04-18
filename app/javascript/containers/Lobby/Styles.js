6f16fd78b0c04e325b1e5a1ac8a83836af6fdb26
Thomas Siller <siller.thomas@gmx.net>
2018-04-18 23:21:57 +0200

    increase font-size

--- app/javascript/containers/Lobby/Styles.js
+++ app/javascript/containers/Lobby/Styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const PlayerCount = styled.div`
+  font-size: 1.5rem;
+  text-shadow: 3px 3px #000000;
   flex: 1;
   align-items: center;
   display: flex;
