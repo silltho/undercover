e61e422d7202cebd34f39ab217d932f532f8dd90
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-04-23 11:09:55 +0200

    recovery

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -7,30 +7,32 @@ class UserChannel < ApplicationCable::Channel
 
   def create_game
     new_game = Game.create
-    new_game.add_player(create_new_player(new_game))
+    p = create_new_player(new_game)
+    #new_game.players << p
     UserChannel.broadcast_to(current_user, type: 'create_game_success', data: new_game.get_game_object)
   end
 
   def leave_game(params)
     game = Game.find(params['id'])
-    player = Player.where(game: game, user: current_user).first.destroy
+    player = Player.where(game: game, user: current_user).destroy_all
     game.players.delete(player)
     UserChannel.broadcast_to(current_user, type: 'leave_game_success', data: game.get_game_object)
     game.broadcast_game_updated
   end
 
   def join_game(params)
-    game = Game.where(code: params['gamecode'], aasm_state: 'waiting').first
-    player = create_new_player(game)
-    game.add_player(player) unless game.players.include? player
+    game = Game.where(code: params['gamecode']).first
+    p = Player.where(game: game, user: current_user).first_or_create
+    p.save!
     UserChannel.broadcast_to(current_user, type: 'join_game_success', data: game.get_game_object)
     game.broadcast_game_updated
+    p.broadcast_player_updated if game.aasm_state != 'waiting'
   end
 
   protected
 
   def create_new_player(game)
-    Player.create(user: current_user, game: game)
+    Player.create!(user: current_user, game_id: game.id)
   end
 
   def get_player(game)
