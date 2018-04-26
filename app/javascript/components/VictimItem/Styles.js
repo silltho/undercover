f73801a380505c0f7b025de16859e18cc347ecf4
Thomas Siller <siller.thomas@gmx.net>
2018-04-26 13:12:42 +0200

    refactor VictimItem into Component + style

---
+++ app/javascript/components/VictimItem/Styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import {
+  WHITE,
+  BLACK,
+  PINK
+} from 'styles/variables'
+import IconFont from 'components/IconFont'
+
+const targetStyle = {
+  backgroundColor: WHITE,
+  color: BLACK
+}
+
+export const VictimItemWrapper = styled.div`
+  display: flex;
+  border: 2px solid ${PINK};
+  padding: 0.5rem;
+  text-align: center;
+  cursor: pointer;
+  margin-top: 0.5rem;
+  ${(props) => props.isTarget && targetStyle}
+  ${(props) => props.isDead && 'opacity: 0.5;'}
+  
+  &:active {
+    ${targetStyle};
+  }
+`
+
+export const FractionImage = styled.img`
+  height: 2.2rem;
+`
+
+export const VictimInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+
+export const VictimRole = styled.span`
+  font-style: italic;
+  opacity: 0.5;
+`
+
+export const VictimStateIcon = styled(IconFont)`
+  font-size: 2rem;
+`
+
+export const VictimIcon = styled.div`
+  display: flex;
+  align-items: center;
+`
