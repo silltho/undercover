f6aaa64880cc260348dbec7e91796ba85aaa0db9
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-04-29 10:37:10 +0200

    user has to enter own nickname

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -5,10 +5,9 @@ class UserChannel < ApplicationCable::Channel
     #current_user.game.broadcast_game_updated unless current_user.game.nil?
   end
 
-  def create_game
+  def create_game(params)
     new_game = Game.create
-    p = create_new_player(new_game)
-    #new_game.players << p
+    create_new_player(new_game, params['nickname'])
     UserChannel.broadcast_to(current_user, type: 'create_game_success', data: new_game.get_game_object)
   end
 
@@ -22,17 +21,18 @@ class UserChannel < ApplicationCable::Channel
   
   def join_game(params)
     game = Game.where(code: params['gamecode']).first
+    nickname = params['nickname']
     UserChannel.broadcast_to(current_user, type: 'wrong_gamecode', data: nil) if game.nil?
     return if game.nil?
     if is_game_running?(game)
-      p = Player.where(game: game, user: current_user).first
+      p = Player.where(game: game, user: current_user, codename: nickname).first
       p.reconnect!
       return false if p.nil?
       p.broadcast_player_updated
     elsif game.full
       return false
     else
-      Player.create!(game: game, user: current_user)
+      Player.create!(game: game, user: current_user, codename: nickname)
     end
 
     UserChannel.broadcast_to(current_user, type: 'join_game_success', data: game.get_game_object)
@@ -45,8 +45,8 @@ class UserChannel < ApplicationCable::Channel
     !game.waiting?
   end
 
-  def create_new_player(game)
-    Player.create!(user: current_user, game_id: game.id)
+  def create_new_player(game, nickname)
+    Player.create!(user: current_user, game_id: game.id, codename: nickname)
   end
 
   def get_player(game)
