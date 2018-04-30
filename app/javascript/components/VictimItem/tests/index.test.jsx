fff8c6ff90e10b70c4fa84f06a6e988097b9435a
Thomas Siller <siller.thomas@gmx.net>
2018-04-30 23:13:17 +0200

    increase coverage

--- app/javascript/components/VictimItem/tests/index.test.jsx
+++ app/javascript/components/VictimItem/tests/index.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { fromJS } from 'immutable'
+import PlayerStates from 'config/playerStates'
 
 import VictimItem from '../index'
 import {
-  VictimItemWrapper
+  VictimItemWrapper,
+  FractionImage
 } from '../Styles'
 
 const defaultProps = {
@@ -20,5 +22,44 @@ describe('<VictimItem />', () => {
   it('should render <VictimItemWrapper>', () => {
     const renderedComponent = renderComponent()
     expect(renderedComponent.find(VictimItemWrapper).length).toEqual(1)
+  })
+
+  it('should render <FractionImage>', () => {
+    const props = {
+      ...defaultProps,
+      victim: fromJS({
+        party: 'mafia'
+      })
+    }
+    const renderedComponent = renderComponent(props)
+    expect(renderedComponent.find(FractionImage).length).toEqual(1)
+  })
+
+  it('should useSkill onClick', () => {
+    const id = '123'
+    const props = {
+      ...defaultProps,
+      victim: fromJS({
+        id,
+        state: PlayerStates.IMPRISONED
+      }),
+      useSkill: jest.fn()
+    }
+    const renderedComponent = renderComponent(props)
+    renderedComponent.find(VictimItemWrapper).prop('onClick')()
+    expect(props.useSkill).toHaveBeenCalledWith(id)
+  })
+
+  it('should not useSkill on dead players', () => {
+    const props = {
+      ...defaultProps,
+      victim: fromJS({
+        state: PlayerStates.DEAD
+      }),
+      useSkill: jest.fn()
+    }
+    const renderedComponent = renderComponent(props)
+    renderedComponent.find(VictimItemWrapper).prop('onClick')()
+    expect(props.useSkill).toHaveBeenCalledTimes(0)
   })
 })
