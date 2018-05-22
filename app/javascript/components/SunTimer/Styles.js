ac527c68a6d313732070e9054c3c049ecd539031
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-22 14:01:12 +0200

    Style SunBody, SunBeams

--- app/javascript/components/SunTimer/Styles.js
+++ app/javascript/components/SunTimer/Styles.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
-import { PINK } from 'styles/variables'
+import {PINK} from 'styles/variables'
 
-const beamHeight = '0.7em'
-const beamWidth = '0.3em'
+const beamHeight = '0.4em'
+const beamWidth = '0.1em'
+const beamRadius = '0.7em'
+const sunColor = 'yellow'
+const sunBodySize = '0.7em'
 
 export const SunBeam = styled.div `
   position: absolute;
@@ -11,10 +14,19 @@ export const SunBeam = styled.div `
   border-style: solid;
   border-width: 0 ${beamWidth} ${beamHeight} ${beamWidth};
   border-radius: 20px;
-  border-color: transparent transparent ${PINK} transparent;
-  transform: rotate(${(props) => props.rotate}deg) translateY(0.9em) rotate(180deg);
+  border-color: transparent transparent ${sunColor} transparent;
+  transform: rotate(${(props) => props.rotate}deg) translateY(${beamRadius}) rotate(180deg);
 `
 
 export const SunBody = styled.div`
   position: relative;
+  background-color: ${sunColor};
+  border-radius: 100%;
+  width: ${sunBodySize};
+  height: ${sunBodySize};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -0.3em;
+  margin-right: -0.1em;
 `
