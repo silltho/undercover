9a727115a782f914e550f46fd480b06a195c02b1
Thomas Siller <siller.thomas@gmx.net>
2018-05-22 11:26:20 +0200

    deactivate Gamecode input and Create Game if no codename is entered

--- app/javascript/components/GameForm/Styles.js
+++ app/javascript/components/GameForm/Styles.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
-import { PINK } from 'styles/variables'
 import Button from 'components/Button'
 
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
   margin: 0.5rem 0;
+  ${(props) => props.deactivated && `
+    opacity: 0.5;
+    pointer-events: none;
+  `}
   
   &:first-child {
     margin-top: 0;
@@ -26,40 +29,8 @@ export const JoinButton = styled(Button)`
   flex: 0;
 `
 
-export const CenteredText = styled.span`
-  flex: 1;
-  text-align: center;
-  opacity: 0.5;
+export const SeperatorRow = styled(Row)`
+  flex-direction: column;
+  margin: 1rem 0;
 `
 
-export const SeperatorWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 1rem 0rem;
-`
-
-export const Seperator = styled.div`
-  border: 1px solid ${PINK};
-  height: 0;
-  position: relative;
-  flex: 1;
-  
-  &:after {
-    content: ' ';
-    border-radius: 50%;
-    border: 0.3rem solid ${PINK};
-    position: absolute;
-    top: -0.25rem;
-    ${(props) => props.right ? { right: '-0.3rem' } : { left: '-0.3rem' }}
-  }
-`
-
-export const SeperatorText = styled.div`
-  color: ${PINK};
-  line-height: 1.5rem;
-  text-align: center;
-  text-transform: uppercase;
-  font-size: 1.5rem;
-  margin: 0 1rem;
-`
