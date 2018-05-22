a5a624fe292f9a1faa238fd2376db4dee6e31254
Thomas Siller <siller.thomas@gmx.net>
2018-05-22 17:53:42 +0200

    increase coverage

--- app/javascript/components/SunTimer/tests/index.test.jsx
+++ app/javascript/components/SunTimer/tests/index.test.jsx
@@ -13,9 +13,18 @@ const renderComponent = (props = defaultProps) => mount(
   <SunTimer {...props} />
 )
 
-describe('<CornerButton />', () => {
+describe('<SunTimer />', () => {
   it('should render an <SunBody> tag', () => {
     const renderedComponent = renderComponent()
     expect(renderedComponent.find(SunBody).length).toEqual(1)
+  })
+
+  it('should componentWillMount', () => {
+    const renderedComponent = renderComponent()
+    jest.useFakeTimers()
+    renderedComponent.instance().componentWillMount()
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 0)
+    jest.runAllTimers()
+    expect(renderedComponent.state().animationStarted).toEqual(true)
   })
 })
