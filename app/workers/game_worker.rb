e5eacd879a9e2541e9a1e8aab48aa773ba1f42fa
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-05-24 09:53:25 +0200

    added check for phase change

--- app/workers/game_worker.rb
+++ app/workers/game_worker.rb
@@ -4,7 +4,7 @@ class GameWorker
 
   def perform(game_id, round)
     g = Game.find(game_id)
-    g.time_is_up if g.round == round
+    g.time_is_up if g.round == round && TimeDifference.between(g.updated_at, Time.now).in_seconds >= 27
   end
 
 end
