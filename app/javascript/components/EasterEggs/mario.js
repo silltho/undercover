2fc3e072107e61e303800303d8c3d92560f904fd
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-24 14:00:17 +0200

    EasterEgg rabbit, ufo, mario

---
+++ app/javascript/components/EasterEggs/mario.js
@@ -0,0 +1,23 @@
+export default function () {
+  const data = 'https://i.imgur.com/QbN03gd.gif'
+
+  const img = new Image()
+  img.src = data
+  img.style.width = '350px'
+  img.style.height = '300px'
+  img.style.transition = '6s all linear'
+  img.style.position = 'fixed'
+  img.style.left = '-400px'
+  img.style.bottom = 'calc(-50% + 330px)'
+  img.style.zIndex = 999999
+
+  document.body.appendChild(img)
+
+  window.setTimeout(() => {
+    img.style.left = 'calc(100% + 500px)'
+  }, 50)
+
+  window.setTimeout(() => {
+    img.parentNode.removeChild(img)
+  }, 6000)
+}
