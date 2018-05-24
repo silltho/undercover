97c31bc5725c3bc622120327563ec564d54d9786
Anna Ronacher <aronacher.mmt-b2015@fh-salzburg.ac.at>
2018-05-24 13:03:07 +0200

    added easter eggs for girls

---
+++ app/javascript/components/EasterEggs/earth.js
@@ -0,0 +1,27 @@
+export default function () {
+  const data = 'https://i.imgur.com/B3zjsAs.gif'
+  const shock = document.createElement('div')
+  const img = new Image()
+  img.src = data
+  img.style.width = '350px'
+  img.style.height = '350px'
+  img.style.transition = '6s all linear'
+  img.style.position = 'fixed'
+  img.style.left = '-400px'
+  img.style.bottom = '0px'
+  img.style.zIndex = 999999
+
+  document.body.appendChild(img)
+
+  // window.setTimeout(function(){
+  //   img.style.left = 'calc(50% - 200px)'
+  // },50)
+
+  window.setTimeout(() => {
+    img.style.left = 'calc(100% + 500px)'
+  }, 50)
+
+  window.setTimeout(() => {
+    img.parentNode.removeChild(img)
+  }, 6000)
+}
