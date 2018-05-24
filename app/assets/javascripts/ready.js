6accf805e54ad23841d27ed32f91e485112b8279
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-25 01:02:54 +0200

    Refactor views scss, Add Start-Button sound

--- app/assets/javascripts/ready.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('turbolinks:load', () => {
-  console.log('Turbo Ready')
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM Ready')
-})
--- app/helpers/landingpages_helper.rb
+++ app/helpers/landingpages_helper.rb
@@ -1,6 +1,5 @@
 module LandingpagesHelper
 
-  # https://rietta.com/blog/2011/12/26/how-to-automate-copyright-notice/
   def copyright_notice_year_range(start_year)
     start_year = start_year.to_i
 
@@ -15,16 +14,35 @@ module LandingpagesHelper
     end
   end
 
-  def init_playbutton()
+  def init_audio()
     javascript_tag "
-    const PLAY_BUTTON = document.getElementsByClassName('play-game-button')[0]
+      const PLAY_BUTTON = document.getElementsByClassName('play-game-button')[0]
+      const TRACK_TAG = document.getElementById('soundtrack')
+      const AUDIO_CONTROL = document.getElementsByClassName('audio-control')[0]
+      const VOLUME_ICON_UP = document.getElementsByClassName('volume-icon-up')[0]
+      const VOLUME_ICON_OFF = document.getElementsByClassName('volume-icon-off')[0]
 
-    function play() {
-      const AUDIO = document.getElementById('play_game')
-      AUDIO.play()
-    }
+      AUDIO_CONTROL.addEventListener('click', () => {
+        if (!TRACK_TAG.muted) {
+          TRACK_TAG.muted = true
+          VOLUME_ICON_UP.classList.add('hide')
+          VOLUME_ICON_OFF.classList.remove('hide')
+        } else {
+          TRACK_TAG.muted = false
+          VOLUME_ICON_UP.classList.remove('hide')
+          VOLUME_ICON_OFF.classList.add('hide')
+        }
+      })
 
-    PLAY_BUTTON.addEventListener('click', play)"
+      function play() {
+        TRACK_TAG.muted = true;
+        const AUDIO = document.getElementById('playsound')
+
+        setTimeout(function() { AUDIO.play() }, 500)
+      }
+
+      PLAY_BUTTON.addEventListener('click', play)
+    "
   end
 end
 
