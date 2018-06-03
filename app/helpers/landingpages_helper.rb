dc2070183c5319518cc27742718e8e007030eb85
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-06-03 20:19:08 +0200

    Update sounds

--- app/helpers/landingpages_helper.rb
+++ app/helpers/landingpages_helper.rb
@@ -16,33 +16,35 @@ module LandingpagesHelper
 
   def init_audio()
     javascript_tag "
-      const PLAY_BUTTON = document.getElementsByClassName('play-game-button')[0]
-      const TRACK_TAG = document.getElementById('soundtrack')
-      const AUDIO_CONTROL = document.getElementsByClassName('audio-control')[0]
-      const VOLUME_ICON_UP = document.getElementsByClassName('volume-icon-up')[0]
-      const VOLUME_ICON_OFF = document.getElementsByClassName('volume-icon-off')[0]
+      document.addEventListener('DOMContentLoaded', function() {
+        const PLAY_BUTTON = document.getElementsByClassName('play-game-button')[0]
+        const TRACK_TAG = document.getElementById('soundtrack')
+        const AUDIO_CONTROL = document.getElementsByClassName('audio-control')[0]
+        const VOLUME_ICON_UP = document.getElementsByClassName('volume-icon-up')[0]
+        const VOLUME_ICON_OFF = document.getElementsByClassName('volume-icon-off')[0]
 
-      AUDIO_CONTROL.addEventListener('click', () => {
-        if (!TRACK_TAG.muted) {
-          TRACK_TAG.muted = true
-          VOLUME_ICON_UP.classList.add('hide')
-          VOLUME_ICON_OFF.classList.remove('hide')
-        } else {
-          TRACK_TAG.muted = false
-          VOLUME_ICON_UP.classList.remove('hide')
-          VOLUME_ICON_OFF.classList.add('hide')
+        AUDIO_CONTROL.addEventListener('click', () => {
+          if (!TRACK_TAG.muted) {
+            TRACK_TAG.muted = true
+            VOLUME_ICON_UP.classList.add('hide')
+            VOLUME_ICON_OFF.classList.remove('hide')
+          } else {
+            TRACK_TAG.muted = false
+            VOLUME_ICON_UP.classList.remove('hide')
+            VOLUME_ICON_OFF.classList.add('hide')
+          }
+        })
+
+        function play() {
+          TRACK_TAG.muted = true;
+          const AUDIO = document.getElementById('playsound')
+
+          setTimeout(function() { AUDIO.play() }, 500)
         }
-      })
 
-      function play() {
-        TRACK_TAG.muted = true;
-        const AUDIO = document.getElementById('playsound')
-
-        setTimeout(function() { AUDIO.play() }, 500)
-      }
-
-      PLAY_BUTTON.addEventListener('click', play)
-    "
+        PLAY_BUTTON.addEventListener('click', play)
+      });
+   "
   end
 end
 
