c8cd56cec1882a1000d3e1955c45b7b5c2f0ed85
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-06-04 14:35:45 +0200

    Require User Interaction to start Audio

--- app/helpers/landingpages_helper.rb
+++ app/helpers/landingpages_helper.rb
@@ -19,32 +19,38 @@ module LandingpagesHelper
       document.addEventListener('DOMContentLoaded', function() {
         const PLAY_BUTTON = document.getElementsByClassName('play-game-button')[0]
         const SOUNDTRACK_BUTTON = document.getElementById('soundtrack')
+        PLAY_BUTTON.addEventListener('click', playStartSound)
 
-        function playStartSound() {
-          SOUNDTRACK_BUTTON.muted = true
+        function playStartSound(e) {
+          e.preventDefault()
           SOUNDTRACK_BUTTON.pause()
           SOUNDTRACK_BUTTON.currentTime = 0
           const AUDIO = document.getElementById('playsound')
+          console.log(AUDIO.duration);
+          AUDIO.play()
+          AUDIO.volume = 0.7
+          setTimeout(function() { window.location.href = '/app' }, (AUDIO.duration * 1000) - 200)
 
-          setTimeout(function() { AUDIO.play() }, 100)
         }
 
-        PLAY_BUTTON.addEventListener('click', playStartSound)
+        const AUDIO_CONTROL = document.getElementById('audio-control')
+        const VOLUME_ICON_UP = document.getElementById('volume-icon-up')
+        const VOLUME_ICON_OFF = document.getElementById('volume-icon-off')
 
-        const AUDIO_CONTROL = document.getElementsByClassName('audio-control')[0]
-        const VOLUME_ICON_UP = document.getElementsByClassName('volume-icon-up')[0]
-        const VOLUME_ICON_OFF = document.getElementsByClassName('volume-icon-off')[0]
-
-        AUDIO_CONTROL.addEventListener('click', () => {
-          if (!SOUNDTRACK_BUTTON.muted) {
-            SOUNDTRACK_BUTTON.muted = true
+        function toggleAudioControl() {
+          if (SOUNDTRACK_BUTTON.paused === false) {
+            SOUNDTRACK_BUTTON.pause()
             VOLUME_ICON_UP.classList.add('hide')
             VOLUME_ICON_OFF.classList.remove('hide')
           } else {
-            SOUNDTRACK_BUTTON.muted = false
+            SOUNDTRACK_BUTTON.play()
             VOLUME_ICON_UP.classList.remove('hide')
             VOLUME_ICON_OFF.classList.add('hide')
           }
+        }
+
+        AUDIO_CONTROL.addEventListener('click', () => {
+          toggleAudioControl()
         })
       });
    "
